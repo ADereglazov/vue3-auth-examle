b148778718deee3113a1a4fe029beab9563836a6
ADereglazov <bt745412>
2022-11-04 13:42:47 +0300

    add ShowHideButton.vue component

---
+++ src/components/ShowHideButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button type="button" class="show-hide-button" aria-label="show/hide password" @click="handleClick">
+    <EyeIcon v-if="show" class="show-hide-button__icon" />
+    <EyeSlashIcon v-else class="show-hide-button__icon" />
+  </button>
+</template>
+
+<script>
+import EyeIcon from "@/assets/icons/eye.svg"
+import EyeSlashIcon from "@/assets/icons/eye-slash.svg"
+
+export default {
+  name: "ShowHideButton",
+  components: { EyeIcon, EyeSlashIcon },
+  emits: ["show-hide"],
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    function handleClick() {
+      emit("show-hide")
+    }
+
+    return {
+      handleClick
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.show-hide-button {
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  &__icon {
+    fill: var(--vt-c-indigo);
+  }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,12 +2,27 @@
   <form class="form">
     <div class="form__wrapper">
       <label for="name" class="form__label">Name</label>
-      <input id="name" type="text" class="form__input">
+      <input
+          id="name"
+          type="text"
+          placeholder="Input username"
+          class="form__input"
+      >
     </div>
 
     <div class="form__wrapper">
       <label for="password" class="form__label">Password</label>
-      <input id="password" type="password" class="form__input">
+      <input
+          id="password"
+          :type="showPassword ? 'text' : 'password'"
+          placeholder="Input password"
+          class="form__input form__input--password"
+      >
+      <ShowHideButton
+        :show="showPassword"
+        class="form__show-password-button"
+        @show-hide="handleShowHide"
+      />
     </div>
 
     <button type="submit" class="form__button">Sign in</button>
@@ -15,8 +30,24 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import ShowHideButton from "@/components/ShowHideButton.vue"
+
 export default {
-  name: "SignIn"
+  name: "SignIn",
+  components: { ShowHideButton },
+  setup() {
+    const showPassword = ref(false);
+
+    function handleShowHide() {
+      showPassword.value = !showPassword.value;
+    }
+
+    return {
+      showPassword,
+      handleShowHide
+    }
+  }
 }
 </script>
 
@@ -27,6 +58,7 @@ export default {
   align-items: center;
 
   &__wrapper {
+    position: relative;
     width: 100%;
     min-width: 320px;
     max-width: 400px;
@@ -37,6 +69,16 @@ export default {
     width: 100%;
     height: 30px;
     padding: 0 10px;
+
+    &--password {
+      padding-right: 30px;
+    }
+  }
+
+  &__show-password-button {
+    position: absolute;
+    right: 0;
+    bottom: 0;
   }
 
   &__button {
