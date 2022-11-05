8c8808f47addf2f9c8e2b2103dc4ce25d4524507
ADereglazov <bt745412>
2022-11-05 22:07:22 +0300

    refactoring composable/field.js

--- src/composable/field.js
+++ src/composable/field.js
@@ -1,7 +1,5 @@
 import { ref, reactive, watch } from "vue";
 
-const not = (val) => !val;
-
 export function useField(field) {
   const valid = ref(true);
   const value = ref(field.value);
@@ -9,18 +7,14 @@ export function useField(field) {
   const errors = reactive({});
 
   const reassign = (val) => {
-    valid.value = true;
     Object.keys(field.validators ?? {}).map((name) => {
       const isValid = field.validators[name](val);
-      errors[name] = not(isValid);
-      if (not(isValid)) {
-        valid.value = false;
-      }
+      errors[name] = !isValid;
+      valid.value = isValid;
     });
   };
 
-  watch(value, reassign);
-  reassign(field.value);
+  watch(value, reassign, { immediate: true });
 
   return { value, valid, errors, touched, blur: () => (touched.value = true) };
 }
